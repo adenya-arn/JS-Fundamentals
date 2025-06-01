6493cdd96496372f07c16520082c3250883acb98
adenya-arn <aadenya7@gmail.com>
2025-06-01 23:39:34 +0300

    1.3 and 6 error fix

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-let line = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing']
 // let i = 0;
 // while(i < line.length){
 //     console.log(line[i]);
